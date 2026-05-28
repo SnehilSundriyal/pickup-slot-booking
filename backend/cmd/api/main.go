985c88c32f1a61560828a20bfcbd405877a84488
snehil <snehilsundriyal@gmail.com>
2026-05-29 00:46:18 +0530

    deployment-handling

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -16,9 +16,17 @@ type application struct {
 }
 
 func main() {
+
 	gin.SetMode(gin.ReleaseMode)
 
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
 	db, err := driver.ConnectSQL()
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,43 +34,22 @@ func main() {
 	repo := dbrepo.NewMysqlRepo(db.SQL)
 
 	err = utils.SeedSlots(db.SQL)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var app application
-	app = application{
+	app := application{
 		DB: repo,
 	}
 
-	port := os.Getenv("PORT")
-	log.Printf("PORT ENV VALUE = %q\n", port)
-
-	if port == "" {
-		port = "8080"
-	}
-
-	log.Println("Creating routes...")
 	server := app.routes()
-	log.Println("Routes created successfully")
 
-	log.Printf("Starting API on port %s...\n", port)
-	log.Println(`
-  ______    ______       ______    ______   __
- /\  ___\  /\  __ \     /\  __ \  /\  == \ /\ \
- \ \ \__\\ \ \ \/\ \    \ \ \_\ \ \ \  __/ \ \ \
-  \ \_____\ \ \_____\    \ \_\ \_\ \ \_\    \ \_\
-   \/_____/  \/_____/     \/_/\/_/  \/_/     \/_/
+	log.Printf("Server starting on port %s\n", port)
 
-`)
-
-	log.Println("Starting Gin server...")
 	err = server.Run(":" + port)
-
-	log.Printf("Gin server exited with error: %v\n", err)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
