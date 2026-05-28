89571549be3033fe9a5c05f80b531269310e466d
snehil <snehilsundriyal@gmail.com>
2026-05-29 00:21:57 +0530

    deployment-handling

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func (app *application) routes() *gin.Engine {
 	router := gin.Default()
-	router.Use(cors.Default())
+	//router.Use(cors.Default())
 
 	router.GET("/slots", app.GetSlots)
 	router.POST("/bookings", app.CreateBooking)
